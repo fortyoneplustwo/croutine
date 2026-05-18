e836deb0c449d97af2525a2d8891663648a1bf69
Olivier Ambroise <oambrois@uwaterloo.ca>
2026-05-18 17:31:54 +0400

    refactor: removes debug logs and favors implicit ptr checks

--- runtime.c
+++ runtime.c
@@ -39,17 +39,13 @@ fiber_t *unwrap(job_t *wrapped) {
 }
 
 void wc_enqueue(void *wc_ptr, fiber_t *f) {
-  printf("wc_enqueue, waiting %d\n", f->id);
   job_t **p = (job_t **)wc_ptr;
-  printf("wc_enqueue2\n");
-  printf("what does p point to? %x\n", *p);
   if (!(*p)) {
-    printf("our waitlist is empty\n");
     *p = wrap(f);
     return;
   }
   job_t *curr = *p;
-  while (curr->next != NULL) {
+  while (curr->next) {
     curr = curr->next;
   }
   f->state = READY;
@@ -190,7 +186,6 @@ int sched_run(void) {
     fiber_t *next = dequeue();
 
     fiber_run(next, NULL);
-    printf("dont running fiber %d\n", next->id);
 
     switch (next->state) {
     case YIELDED:
@@ -227,12 +222,10 @@ void switch_context_as_entry(void *arg) {
   context_t *new = (context_t *)arg;
   sched->curr->state = DEAD;
   sched->curr = NULL;
-  printf("switch entry\n");
   switch_context(&old, new);
 }
 
 void fiber_await(fiber_t *f) {
-  printf("inside await\n");
   // 'We' == the caller of fiber_await()
   // =========================================================================
   // If we are a fiber
@@ -269,21 +262,14 @@ void fiber_await(fiber_t *f) {
     context_t initiator_ctx;
     self = fiber_create((void *)switch_context_as_entry, (void *)&initiator_ctx,
                         1);
-    printf("created fiber self %d\n", self->id);
     wc_enqueue(&f->waitlist, self);
     self->state = BLOCKED;
     switch_context(&initiator_ctx, &sched->self->context);
-    printf("returning from await\n");
     fiber_destroy(self);
     return;
   }
-  printf("we are already a fiber\n");
-  printf("supposed to wait for fiber %d\n", f->id);
-  printf("we are fiber %d\n", self->id);
   wc_enqueue(&f->waitlist, self);
-  printf("about to set %d's state to blocked\n", self->id);
   self->state = BLOCKED;
-  printf("was able to set %d's state to blocked\n", self->id);
   switch_context(&self->context, &sched->self->context);
   return;
 }
