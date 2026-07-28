faac6712f71f3a982c073840a5fca91d16baac56
Olivier Ambroise <oambrois@uwaterloo.ca>
2026-07-28 19:28:45 +0400

    feat: comment out printfs for precise profiling

--- scheduler.c
+++ scheduler.c
@@ -91,9 +91,9 @@ int sched_init() {
     iorequests[i].writersq = rbuf_init(RBUFDEFAULTCAP);
   }
   sched->self = fiber_create(sched_run, NULL, 0);
-  printf("created scheduler fiber with id %d\n", sched->self->id);
+  // printf("created scheduler fiber with id %d\n", sched->self->id);
   fiber_t *npfiber = fiber_create(np_run, NULL, -1);
-  printf("created netpoller fiber with id %d\n", npfiber->id);
+  // printf("created netpoller fiber with id %d\n", npfiber->id);
   npfiber->state = READY;
   err = rb_enqueue(sched->runq, npfiber);
   if (err) {
